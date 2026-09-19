559e3440d0d48c5ce931547242f784ec6aa4f10c
Sobuj <sobujmadbor660@gmail.com>
2026-09-19 22:05:17 +0600

    Refactor FeaturedBooks component layout with updated grid structure and button styling

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -12,13 +12,13 @@ const Featured = await FeaturedData();
        <div className='max-w-7xl mx-auto px-10 py-10'>
  <h1 className='font-bold text-3xl my-3'>Featured Books</h1>  
 
- <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  gap-3'>
+ <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4  gap-3'>
 {
 Featured?.map(Features => <BookCard key={Features?._id} book={Features}></BookCard>) 
 }
 </div> 
 <div className='flex items-center justify-center pt-2'>
-  <Link href="/books"><Button variant='outline'>View All</Button></Link>
+  <Link href="/books"><Button variant='outline' className="rounded-lg mt-4">View All</Button></Link>
 </div>
                     </div>
     );
